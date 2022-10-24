daed50a6a8b180fcdca4dad75be5538f1c233755
Victoria Karami <pallada1985@gmail.com>
2022-10-24 14:20:44 -0400

    Adding Customer class

---
+++ BackEnd/src/main/java/com/revature/models/Customer.java
@@ -0,0 +1,13 @@
+package com.revature.models;
+
+
+public class Customer{
+
+    // Properties
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
