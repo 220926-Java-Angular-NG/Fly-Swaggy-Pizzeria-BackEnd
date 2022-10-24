35335beb7fde40709073dc4806436ecfd86a926e
Victoria Karami <pallada1985@gmail.com>
2022-10-24 15:08:50 -0400

    Adding model classes Pizza and Customer

---
+++ BackEnd/src/main/java/org.swagarelli/models/Customer.java
@@ -0,0 +1,29 @@
+package org.swagarelli.models;
+
+
+public class Customer{
+
+    // Properties
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+    private boolean isMember;
+
+    // Constructors
+    // No-arg constructor
+    public Customer() {
+        id = 0;
+        firstName = "";
+        lastName = "";
+        email = "";
+        password = "";
+        address = "";
+        isMember = false;
+    }
+
+    // Overloading the constructor with the
+
+}
