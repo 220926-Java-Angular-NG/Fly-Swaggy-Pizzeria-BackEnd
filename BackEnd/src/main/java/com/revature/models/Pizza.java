197c00e5679f487f0921eb36a5704ce6b8efc841
Pallada1985 <98477378+Pallada1985@users.noreply.github.com>
2022-10-24 13:13:07 -0400

    adding a model class Pizza

---
+++ BackEnd/src/main/java/com/revature/models/Pizza.java
@@ -0,0 +1,14 @@
+package com.revature.models;
+
+import java.util.ArrayList;
+
+// Pizza class is a model class for the Pizzeria project.
+//
+public class Pizza {
+
+    // Properties
+    private String crust;
+    private String sauce;
+    private String cheese;
+    private ArrayList<String> toppings;
+}
