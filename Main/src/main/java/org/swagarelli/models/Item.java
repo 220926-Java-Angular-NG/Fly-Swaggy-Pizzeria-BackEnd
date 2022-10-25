91edb1e3c0cb17809c5a0ba91f110e6cb6bcbec9
Logan <logan.tan@wsu.edu>
2022-10-25 13:42:29 -0700

    Adding in Connection manager, and Constructors for the Item Class

--- Main/src/main/java/org/swagarelli/models/Item.java
+++ Main/src/main/java/org/swagarelli/models/Item.java
@@ -3,6 +3,15 @@ package org.swagarelli.models;
 public class Item {
     double price;
 
+
+    public Item() {
+        this.price = 0;
+    }
+
+    public Item(double price) {
+        this.price = price;
+    }
+
     public double getPrice() {
         return price;
     }
