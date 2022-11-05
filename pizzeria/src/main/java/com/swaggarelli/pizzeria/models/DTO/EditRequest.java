69763c55af345ae1ca7d6fbb86c7ca278089c363
Logan <logan.tan@wsu.edu>
2022-11-05 10:26:14 -0700

    More DTO updates

--- pizzeria/src/main/java/com/swaggarelli/pizzeria/models/DTO/EditRequest.java
+++ pizzeria/src/main/java/com/swaggarelli/pizzeria/models/DTO/EditRequest.java
@@ -1,4 +1,17 @@
 package com.swaggarelli.pizzeria.models.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditRequest {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private String address2;
+    private String zipCode;
 }
--- pizzeria/src/main/java/com/swaggarelli/pizzeria/models/DTO/LoginResponse.java
+++ pizzeria/src/main/java/com/swaggarelli/pizzeria/models/DTO/LoginResponse.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
-    String Username;
-    String Password;
+public class LoginResponse {
+    int id;
 }
